b4dd049c59dd26879fcb82795d019ca5ddc89ff6
BregNights <daniel_baumann567@hotmail.com>
2025-02-22 19:15:12 -0300

    estudos de sabado

--- database-postgres.js
+++ database-postgres.js
@@ -1,40 +1,42 @@
-import { sql } from './db-connect.js'
+import { sql } from "./db-connect.js";
 
 export class DatabasePostgres {
-    async create(user) {
-        const timestamp = new Date
-        const { name, email, password } = user
-        await sql `insert into users (name, email, password, created_at) VALUES (${name}, ${email}, ${password}, ${timestamp})`
+  async create(user) {
+    const timestamp = new Date();
+    const { name, email, password } = user;
+    await sql`insert into users (name, email, password, created_at) VALUES (${name}, ${email}, ${password}, ${timestamp})`;
+  }
+
+  async list(search) {
+    let users;
+
+    if (search) {
+      users = await sql`select * from users WHERE name ilike ${
+        "%" + search + "%"
+      }`;
+    } else {
+      users = await sql`select * from users`;
     }
 
-    async list(search) {
-        let users
+    return users;
+  }
 
-        if (search) {
-            users = await sql`select * from users WHERE name ilike ${'%' + search + '%'}`
-        } else {
-            users = await sql`select * from users`
-        }
+  async findByEmail(email) {
+    const users = await sql`SELECT * FROM users WHERE email = ${email} LIMIT 1`;
+    return users[0] || null;
+  }
 
-        return users
-    }
+  async findById(id) {
+    const users = await sql`SELECT * FROM users WHERE id = ${id} LIMIT 1`;
+    return users[0] || null;
+  }
 
-    async findByEmail(email) {
-        const users = await sql`SELECT * FROM users WHERE email = ${email} LIMIT 1`
-        return users[0] || null
-    }
+  async update(id, user) {
+    const { name, password } = user;
+    await sql`update users set name = ${name}, password = ${password} WHERE id = ${id}`;
+  }
 
-    async findById(id) {
-        const users = await sql`SELECT * FROM users WHERE id = ${id} LIMIT 1`
-        return users[0] || null
-    }
-
-    async update(id, user) {
-        const { name, email, password } = user
-        await sql`update users set name = ${name}, email = ${email}, password = ${password} WHERE id = ${id}`
-    }
-
-    async delete (id) {
-        await sql`DELETE FROM users WHERE id = ${id}`
-    }
+  async delete(id) {
+    await sql`DELETE FROM users WHERE id = ${id}`;
+  }
 }
